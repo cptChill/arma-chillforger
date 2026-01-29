e901a161c24fc4fcefb22b68e61f11c5cbf806e8
cptChill <p.keil.bb@gmail.com>
2026-01-29 21:37:59 +0100

    Added "ActionManager.conf"

--- Scripts/check_for_ammo_logic.c
+++ Scripts/check_for_ammo_logic.c
@@ -1,33 +1,44 @@
-void CheckNearbyAmmo()
+void ScanForRelevantAmmo()
 {
+    // 1. Get the player and their current weapon
     IEntity player = GetGame().GetPlayerController().GetControlledEntity();
     if (!player) return;
 
-    vector center = player.GetOrigin();
-    float radius = 10.0;
+    CharacterControllerComponent charCtrl = CharacterControllerComponent.Cast(player.FindComponent(CharacterControllerComponent));
+    BaseWeaponManagerComponent weaponMgr = BaseWeaponManagerComponent.Cast(player.FindComponent(BaseWeaponManagerComponent));
     
-    // 1. Get current weapons
-    InventoryStorageManagerComponent storage = InventoryStorageManagerComponent.Cast(player.FindComponent(InventoryStorageManagerComponent));
-    array<IEntity> weapons = {};
-    storage.GetItems(weapons); // Simplified: you'd filter for WeaponEntities
+    if (!weaponMgr) return;
     
-    bool foundAmmo = false;
-    string report = "Nearby Ammo Check:\n";
+    // Get the currently selected weapon
+    WeaponComponent currentWeapon = weaponMgr.GetCurrentWeapon();
+    if (!currentWeapon) return;
 
-    // 2. Search the area
-    GetGame().GetWorld().QueryEntitiesBySphere(center, radius,  void(IEntity ent)
+    // Get the class name of the magazine this weapon needs
+    // This is the "Key" we use to find matching items on the ground
+    string requiredMagClassName = currentWeapon.GetDefaultMagazinePrefabName();
+
+    string report = "Scanning for: " + requiredMagClassName + "\n";
+    bool foundMatch = false;
+
+    // 2. Spatial Query (10m radius)
+    GetGame().GetWorld().QueryEntitiesBySphere(player.GetOrigin(), 10.0,  void(IEntity ent)
     {
-        // Check if entity is a magazine
+        // Check if the item is a magazine
         MagazineComponent magComp = MagazineComponent.Cast(ent.FindComponent(MagazineComponent));
         if (magComp)
         {
-            // Logic to compare magComp.GetUIInfo().GetName() with weapon requirements
-            foundAmmo = true;
-            report += "- Found: " + ent.GetPrefabData().GetPrefabName() + "\n";
+            // Does this ammo match our gun?
+            if (ent.GetPrefabData().GetPrefabName() == requiredMagClassName)
+            {
+                foundMatch = true;
+                report += "[MATCH] Found at: " + ent.GetOrigin().ToString() + "\n";
+            }
         }
-        return true; // Continue search
-    }, null, EQueryEntitiesFlags.DYNAMIC | EQueryEntitiesFlags.STATIC);
+        return true;
+    }, null, EQueryEntitiesFlags.DYNAMIC);
 
-    // 3. Output to UI
-    SCR_HintManagerComponent.GetInstance().ShowCustomHint(report, "Ammo Scanner", 5.0);
+    if (!foundMatch) report += "No matching ammo nearby.";
+
+    // 3. Popup result
+    SCR_HintManagerComponent.GetInstance().ShowCustomHint(report, "Ammo Scanner", 7.0);
 }
