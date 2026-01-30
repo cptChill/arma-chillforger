46381e3591647bd2aca4d624a89c09d3e9041940
cptChill <p.keil.bb@gmail.com>
2026-01-30 09:38:02 +0100

    deleted empty file "MY_AmmoCheckAction"

--- Scripts/Game/MY_PlayerController.c
+++ Scripts/Game/MY_PlayerController.c
@@ -1,14 +1,21 @@
 modded class SCR_PlayerController : PlayerController
 {
-	// This function intercepts inputs mapped in your .conf file
-	override void OnAction(string actionName, float value, EActionTrigger triggerKind)
-	{
-		super.OnAction(actionName, value, triggerKind);
-		
-		// This string must match the 'Action Name' in your AmmoscannerInputs.conf
-		if (actionName == "AmmoScanner" && triggerKind == EActionTrigger.DOWN)
-		{
-			MyAmmoScannerLogic.ScanForRelevantAmmo();
-		}
-	}
+    override void OnAction(string actionName, float value, EActionTrigger triggerKind)
+    {
+        super.OnAction(actionName, value, triggerKind);
+        
+        if (actionName == "AmmoScanner" && triggerKind == EActionTrigger.DOWN)
+        {
+            MyAmmoScannerLogic.ScanForRelevantAmmo();
+        }
+    }
+
+    // Ensure our custom input context is actually active
+    override void OnPostInit(IEntity owner)
+    {
+        super.OnPostInit(owner);
+        
+        // This makes sure the engine is actually listening for your specific .conf setup
+        GetInputManager().AddContext("IngameContext");
+    }
 }
